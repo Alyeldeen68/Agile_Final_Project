4fa8d0591da83e051ae85ed29bc74b2cb1de5edb
Alyeldeen68 <alyeldeenkhaled2@gmail.com>
2022-12-16 22:07:06 +0200

    login-endpoint

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,4 +1,4 @@
-import react from "react";
+import react, { useState } from "react";
 import "./Login.css";
 import angelImg from "../../Imgs/4321520.png";
 import Button from "react-bootstrap/Button";
@@ -6,8 +6,21 @@ import Form from "react-bootstrap/Form";
 import leftButton from "../../Imgs/turn-left.png";
 import { RouterLink } from "../../upYouGo/Styling";
 import { useNavigate } from "react-router-dom";
+import axios from "axios";
+
 const Login = () => {
   const navigate = useNavigate();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const handleSubmit = async () => {
+    await axios
+      .post("https://dawi.onrender.com/login", {
+        email: email,
+        password: password,
+      })
+      .then((response) => console.log(response))
+      .catch((err) => console.log(err));
+  };
   return (
     <div className="main-login">
       <div className="login-container">
@@ -21,19 +34,23 @@ const Login = () => {
           <Form>
             <Form.Group className="mb-3" controlId="formBasicEmail">
               <Form.Label>Email address</Form.Label>
-              <Form.Control type="email" placeholder="Enter email" />
+              <Form.Control
+                type="email"
+                onChange={(e) => setEmail(e.target.value)}
+                placeholder="Enter email"
+              />
             </Form.Group>
 
             <Form.Group className="mb-3" controlId="formBasicPassword">
               <Form.Label>Password</Form.Label>
-              <Form.Control type="password" placeholder="Password" />
+              <Form.Control
+                type="password"
+                onChange={(e) => setPassword(e.target.value)}
+                placeholder="Password"
+              />
             </Form.Group>
 
-            <Button
-              onClick={() => navigate("/home-page")}
-              variant="primary"
-              type="submit"
-            >
+            <Button onClick={handleSubmit} variant="primary" type="submit">
               Login
             </Button>
           </Form>
