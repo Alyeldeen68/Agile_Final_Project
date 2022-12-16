8cdc23c48076e842acc53643fed4069289b9cac8
Alyeldeen68 <alyeldeenkhaled2@gmail.com>
2022-12-16 22:11:36 +0200

    login-endpoint

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -13,6 +13,7 @@ const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const handleSubmit = async () => {
+    alert("Hello");
     await axios
       .post("https://dawi.onrender.com/login", {
         email: email,
