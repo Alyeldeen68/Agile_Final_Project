c53d3e3962dc605f31afb6c76c51d89a20dd24cd
Alyeldeen68 <alyeldeenkhaled2@gmail.com>
2022-12-28 19:23:06 +0200

    add-reservations-test

--- src/Components/Home Page/HomePage.js
+++ src/Components/Home Page/HomePage.js
@@ -142,6 +142,7 @@ const HomePage = () => {
       },
       body: JSON.stringify(reservationData),
     })
+      .then((response) => response.json())
       .then((response) => console.log(response))
       .catch((err) => console.log(err));
 
