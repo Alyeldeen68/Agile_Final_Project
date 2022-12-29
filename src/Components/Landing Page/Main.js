09f93dc9db68b839bda253e28f4d43404698f290
Alyeldeen68 <alyeldeenkhaled2@gmail.com>
2022-12-29 20:40:48 +0200

    sidebar-css

--- src/Components/Landing Page/Main.js
+++ src/Components/Landing Page/Main.js
@@ -23,7 +23,9 @@ const Main = () => {
           <Card style={{ width: "18rem", borderRadius: "1rem" }}>
             <Card.Img className="card-img" variant="top" src={doctorImg} />
             <Card.Body>
-              <div style={{ display: "flex", flexDirection: "column" }}>
+              <div
+                style={{ display: "flex", flexDirection: "column", gap: "4%" }}
+              >
                 <div>
                   <Card.Title style={{ textAlign: "center" }}>
                     Doctor
@@ -57,24 +59,32 @@ const Main = () => {
           <Card style={{ width: "18rem", borderRadius: "1rem" }}>
             <Card.Img className="card-img" variant="top" src={pharmacistImg} />
             <Card.Body>
-              <Card.Title style={{ textAlign: "center" }}>
-                Pharmacist
-              </Card.Title>
-              <Card.Text>
-                Some quick example text to build on the card title and make up
-                the bulk of the card's content.
-              </Card.Text>
-              <div className="card-button">
-                <RouterLink to="/doctor-form">
-                  <Button
-                    onClick={() => {
-                      dispatch(signUp("Pharmacist"));
-                    }}
-                    variant="primary"
-                  >
-                    Register
-                  </Button>
-                </RouterLink>
+              <div
+                style={{ display: "flex", flexDirection: "column", gap: "4%" }}
+              >
+                <div>
+                  <Card.Title style={{ textAlign: "center" }}>
+                    Pharmacist
+                  </Card.Title>
+                </div>
+                <div>
+                  <Card.Text>
+                    Some quick example text to build on the card title and make
+                    up the bulk of the card's content.
+                  </Card.Text>
+                </div>
+                <div className="card-button">
+                  <RouterLink to="/doctor-form">
+                    <Button
+                      onClick={() => {
+                        dispatch(signUp("Pharmacist"));
+                      }}
+                      variant="primary"
+                    >
+                      Register
+                    </Button>
+                  </RouterLink>
+                </div>
               </div>
             </Card.Body>
           </Card>
